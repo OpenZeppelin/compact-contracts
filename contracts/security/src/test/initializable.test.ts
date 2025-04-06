f404601793a28b5698f486b855a1fafbd145ea1b
andrew <fleming-andrew@protonmail.com>
2025-04-06 16:33:51 -0500

    fix initializable tests

--- contracts/security/src/test/initializable.test.ts
+++ contracts/security/src/test/initializable.test.ts
@@ -2,32 +2,35 @@ import { it, describe, expect } from '@jest/globals';
 import { InitializableSimulator } from './simulators/InitializableSimulator.js';
 import { Initializable_STATE as STATE } from '../artifacts/MockInitializable/contract/index.cjs';
 
-
-const contract = new InitializableSimulator();
+let initializable: InitializableSimulator;
 
 describe('Initializable', () => {
+  beforeEach(() => {
+    initializable = new InitializableSimulator();
+  });
+
   it('should generate the initial ledger state deterministically', () => {
-    const contract2 = new InitializableSimulator();
-    expect(contract.getCurrentPublicState()).toEqual(contract2.getCurrentPublicState());
+    const initializable2 = new InitializableSimulator();
+    expect(initializable.getCurrentPublicState()).toEqual(initializable2.getCurrentPublicState());
   });
 
   describe('initialize', () => {
     it('should not be initialized', () => {
-      expect(contract.isInitialized()).toEqual(false);
-      expect(contract.getCurrentPublicState().initializableState).toEqual(STATE.uninitialized);
+      expect(initializable.isInitialized()).toEqual(false);
+      expect(initializable.getCurrentPublicState().initializableState).toEqual(STATE.uninitialized);
     });
 
     it('should initialize', () => {
-      contract.initialize();
-      expect(contract.isInitialized()).toEqual(true);
-      expect(contract.getCurrentPublicState().initializableState).toEqual(STATE.initialized);
+      initializable.initialize();
+      expect(initializable.isInitialized()).toEqual(true);
+      expect(initializable.getCurrentPublicState().initializableState).toEqual(STATE.initialized);
       });
     });
 
     it('should fail when re-initialized', () => {
       expect(() => {
-          contract.initialize();
-          contract.initialize();
+        initializable.initialize();
+        initializable.initialize();
       }).toThrow('Contract already initialized');
   });
 });
