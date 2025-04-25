7a5ad93249eeece94a2def31b491bb9344017df5
Emanuel Solis <esolis6114@gmail.com>
2025-04-25 17:56:06 -0400

    Update witness function signature

--- contracts/erc721/src/witnesses/ERC721Witnesses.ts
+++ contracts/erc721/src/witnesses/ERC721Witnesses.ts
@@ -1,3 +1,21 @@
-// This is how we type an empty object.
-export type ERC721PrivateState = Record<string, never>;
-export const ERC721Witnesses = {};
+import * as Contract from "../artifacts/MockERC721/contract/index.cjs";
+import { WitnessContext } from "@midnight-ntwrk/compact-runtime";
+
+export type ERC721PrivateState = {
+   tokenURI: string
+};
+
+export const ERC721Witnesses = {
+   /**
+    * @description A mock implementation of the computeTokenURI witness function
+    *
+    * @dev This function should be modified to meet the needs of your application
+    * 
+    * @param {ERC721PrivateState} privateState The private state of the contract
+    * @param {string} baseURI The baseURI which is the empty string by default
+    * @returns {[ERC721PrivateState, string]} A tuple of the new private state and the declared return value
+    */
+   computeTokenURI({ privateState }: WitnessContext<Contract.Ledger, ERC721PrivateState>, baseURI: string, tokenId: bigint): [ERC721PrivateState, string] {
+      return [privateState, baseURI + privateState.tokenURI + tokenId.toString()];
+   }
+}
