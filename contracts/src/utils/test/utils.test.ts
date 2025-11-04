42b2b9edd04b3e3d93f219c343f84a70d1d67bb6
⟣ €₥ℵ∪ℓ ⟢ <34749913+emnul@users.noreply.github.com>
2025-11-03 20:21:43 -0500

    biome lint fix

--- contracts/src/utils/test/utils.test.ts
+++ contracts/src/utils/test/utils.test.ts
@@ -27,7 +27,9 @@ describe('Utils', () => {
     });
 
     it('should not return zero for a zero contract address', () => {
-      expect(contract.isKeyOrAddressZero(contractUtils.ZERO_ADDRESS)).toBe(true);
+      expect(contract.isKeyOrAddressZero(contractUtils.ZERO_ADDRESS)).toBe(
+        true,
+      );
     });
   });
 
