62a75ca35535d98d6870c751d77ac69e7aeb7a6d
⟣ €₥ℵ∪ℓ ⟢ <34749913+emnul@users.noreply.github.com>
2026-02-04 00:39:31 -0500

    Refactor PausableSimulator

--- contracts/src/security/witnesses/PausableWitnesses.ts
+++ contracts/src/security/witnesses/PausableWitnesses.ts
@@ -1,6 +1,34 @@
 // SPDX-License-Identifier: MIT
 // OpenZeppelin Compact Contracts v0.0.1-alpha.1 (security/witnesses/PausableWitnesses.ts)
 
-// This is how we type an empty object.
-export type PausablePrivateState = Record<string, never>;
-export const PausableWitnesses = {};
+import type { WitnessContext } from '@midnight-ntwrk/compact-runtime';
+import type { Ledger } from '../../../artifacts/MockPausable/contract/index.js';
+
+/**
+ * @description Interface defining the witness methods for Pausable operations.
+ * @template P - The private state type.
+ */
+export interface IPausableWitnesses<P> { }
+
+
+export type PausablePrivateState = {};
+
+/**
+ * @description Utility object for managing the private state of a Pausable contract.
+ */
+export const PausablePrivateState = {
+  /**
+   * @description Generates a new private state
+   * @returns An empty PausablePrivateState instance.
+   */
+  generate: (): PausablePrivateState => {
+    return {};
+  },
+};
+
+/**
+ * @description Factory function creating witness implementations for Pausable operations.
+ * @returns An object implementing the Witnesses interface for PausablePrivateState.
+ */
+export const PausableWitnesses =
+  (): IPausableWitnesses<PausablePrivateState> => ({});
