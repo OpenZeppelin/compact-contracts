24af2c39d559c2e0cd97c8cb12812bf631a88694
⟣ €₥ℵ∪ℓ ⟢ <34749913+emnul@users.noreply.github.com>
2026-02-04 15:12:47 -0500

    Refactor token Witness files

--- contracts/src/token/witnesses/NonFungibleTokenWitnesses.ts
+++ contracts/src/token/witnesses/NonFungibleTokenWitnesses.ts
@@ -1,6 +1,31 @@
 // SPDX-License-Identifier: MIT
 // OpenZeppelin Compact Contracts v0.0.1-alpha.1 (token/witnesses/NonFungibleToken.ts)
 
-// This is how we type an empty object.
-export type NonFungibleTokenPrivateState = Record<string, never>;
-export const NonFungibleTokenWitnesses = {};
+/**
+ * @description Interface defining the witness methods for NonFungibleToken operations.
+ * @template P - The private state type.
+ */
+export interface INonFungibleTokenWitnesses<P> { }
+
+
+export type NonFungibleTokenPrivateState = {};
+
+/**
+ * @description Utility object for managing the private state of a NonFungibleToken contract.
+ */
+export const NonFungibleTokenPrivateState = {
+  /**
+   * @description Generates a new private state
+   * @returns An empty NonFungibleTokenPrivateState instance.
+   */
+  generate: (): NonFungibleTokenPrivateState => {
+    return {};
+  },
+};
+
+/**
+ * @description Factory function creating witness implementations for NonFungibleToken operations.
+ * @returns An object implementing the Witnesses interface for NonFungibleTokenPrivateState.
+ */
+export const NonFungibleTokenWitnesses =
+  (): INonFungibleTokenWitnesses<NonFungibleTokenPrivateState> => ({});
