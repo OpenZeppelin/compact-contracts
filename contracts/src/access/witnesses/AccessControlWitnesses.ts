7511ab703ee13bd678577332bb66023184321a8a
⟣ €₥ℵ∪ℓ ⟢ <34749913+emnul@users.noreply.github.com>
2026-02-05 15:42:11 -0500

    Refactor AccessControl, Ownable Witnesses

--- contracts/src/access/witnesses/AccessControlWitnesses.ts
+++ contracts/src/access/witnesses/AccessControlWitnesses.ts
@@ -1,6 +1,31 @@
 // SPDX-License-Identifier: MIT
 // OpenZeppelin Compact Contracts v0.0.1-alpha.1 (access/witnesses/AccessControlWitnesses.ts)
 
-// This is how we type an empty object.
-export type AccessControlPrivateState = Record<string, never>;
-export const AccessControlWitnesses = {};
+/**
+ * @description Interface defining the witness methods for AccessControl operations.
+ * @template P - The private state type.
+ */
+export interface IAccessControlWitnesses<P> { }
+
+
+export type AccessControlPrivateState = {};
+
+/**
+ * @description Utility object for managing the private state of a AccessControl contract.
+ */
+export const AccessControlPrivateState = {
+  /**
+   * @description Generates a new private state
+   * @returns An empty AccessControlPrivateState instance.
+   */
+  generate: (): AccessControlPrivateState => {
+    return {};
+  },
+};
+
+/**
+ * @description Factory function creating witness implementations for AccessControl operations.
+ * @returns An object implementing the Witnesses interface for AccessControlPrivateState.
+ */
+export const AccessControlWitnesses =
+  (): IAccessControlWitnesses<AccessControlPrivateState> => ({});
--- contracts/src/access/witnesses/OwnableWitnesses.ts
+++ contracts/src/access/witnesses/OwnableWitnesses.ts
@@ -1,6 +1,31 @@
 // SPDX-License-Identifier: MIT
 // OpenZeppelin Compact Contracts v0.0.1-alpha.1 (access/witnesses/OwnableWitnesses.ts)
 
-// This is how we type an empty object.
-export type OwnablePrivateState = Record<string, never>;
-export const OwnableWitnesses = {};
+/**
+ * @description Interface defining the witness methods for Ownable operations.
+ * @template P - The private state type.
+ */
+export interface IOwnableWitnesses<P> { }
+
+
+export type OwnablePrivateState = {};
+
+/**
+ * @description Utility object for managing the private state of a Ownable contract.
+ */
+export const OwnablePrivateState = {
+  /**
+   * @description Generates a new private state
+   * @returns An empty OwnablePrivateState instance.
+   */
+  generate: (): OwnablePrivateState => {
+    return {};
+  },
+};
+
+/**
+ * @description Factory function creating witness implementations for Ownable operations.
+ * @returns An object implementing the Witnesses interface for OwnablePrivateState.
+ */
+export const OwnableWitnesses =
+  (): IOwnableWitnesses<OwnablePrivateState> => ({});
