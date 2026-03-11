66cd730088b21ceca134128a5171fb2ed11a5220
andrew <fleming-andrew@protonmail.com>
2026-03-11 01:48:41 -0300

    add empty witnesses

---
+++ contracts/src/multisig/witnesses/UnshieldedTreasuryWitness.ts
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MIT
+// OpenZeppelin Compact Contracts v0.0.1-alpha.1 (multisig/witnesses/UnshieldedTreasuryWitness.ts)
+
+export type UnshieldedTreasuryPrivateState = Record<string, never>;
+export const UnshieldedTreasuryPrivateState: UnshieldedTreasuryPrivateState = {};
+export const UnshieldedTreasuryWitnesses = () => ({});
